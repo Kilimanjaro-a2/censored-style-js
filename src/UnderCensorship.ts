cc8bc185550e0bae344269fe9863388985d7c156
Kili <kilimanjaro.a2@gmail.com>
2021-07-16 03:23:46 +0900

    Added some type information

--- src/UnderCensorship.ts
+++ src/UnderCensorship.ts
@@ -1,41 +1,40 @@
+type censorshipType = "paint" | "blur" | "visible"
+
 class UnderCensorship extends HTMLElement {
-    #defaultElement = "censored"
-    #defaultType = "paint"
-    #defaultColor = "black"
+    #defaultElement: string = "censored"
+    #defaultType: censorshipType = "paint"
+    #defaultColor: string = "black"
 
     constructor() {
         /*
          * Initialize
          */
         super()
-        const shadow = this.attachShadow({ mode: "open" })
-        const wrapper = document.createElement("span")
-        const slot = document.createElement("slot")
+        const shadow: ShadowRoot = this.attachShadow({ mode: "open" })
+        const wrapper: HTMLElement = document.createElement("span")
+        const slot: HTMLSlotElement = document.createElement("slot")
         wrapper.setAttribute("class", "container")
         wrapper.appendChild(slot)
 
         /*
          * Tag option
          */
-        const elementAttribute = this.getAttribute('tag')
-        const censorshipElement = elementAttribute != null && elementAttribute != ""
-                ? elementAttribute 
-                : this.#defaultElement
+        const censorshipElement: string = this.getAttribute('tag') ?? this.#defaultElement
 
         /*
          * Type option
          */
-        const typeAttribute = this.getAttribute('type') ?? "paint"
-        const typeSet = new Set(["paint", "blur", "visible"])
-        const censorshipType = typeSet.has(typeAttribute) ? typeAttribute : this.#defaultType
+        const attrType: string = this.getAttribute('type') ?? "paint"
+        const typeSet: Set<string> = new Set(["paint", "blur", "visible"]) // TODO: Use TypeScript's type
+        const censorshipType: censorshipType = typeSet.has(attrType) ? attrType as censorshipType : this.#defaultType
 
         /*
          * Style
          */
-        const colorAttribute = this.getAttribute('color') ?? "#000000"
-        const isColorCode = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$").test(colorAttribute)
-        const censorshipColor = isColorCode ? colorAttribute : this.#defaultColor;
-        const style = document.createElement("style")
+        const colorAttribute: string = this.getAttribute('color') ?? "#000000"
+        const isColorCode: boolean = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$").test(colorAttribute)
+        const censorshipColor: string = isColorCode ? colorAttribute : this.#defaultColor;
+        const style: HTMLStyleElement = document.createElement("style")
         style.textContent = this.#generateStyle(censorshipType, censorshipElement, censorshipColor)
     
         /*
@@ -47,13 +46,13 @@ class UnderCensorship extends HTMLElement {
         /*
          * Replace
          */
-        const replaceTextAttribute = this.getAttribute('replace-text')
-        if (replaceTextAttribute != null && replaceTextAttribute != "") {
-            const replaceRepeatAttribute = this.getAttribute('replace-repeat')
-            const replaceRepeat = replaceRepeatAttribute == "true" || replaceRepeatAttribute == "True" 
+        const replaceTextAttribute: string = this.getAttribute('replace-text') ?? ""
+        if (replaceTextAttribute != "") {
+            const replaceRepeatAttribute: string = this.getAttribute('replace-repeat') ?? ""
+            const replaceRepeat: boolean = replaceRepeatAttribute == "true" || replaceRepeatAttribute == "True" 
 
             // It won't work unless you add child elements to the Shadow DOM first.
-            const htmlCollections = slot.assignedElements()
+            const htmlCollections: HTMLCollectionOf<Element>[] = slot.assignedElements()
                 .map(elm => elm.getElementsByTagName(censorshipElement))
                 .filter(col => col.length > 0)
 
@@ -70,7 +69,11 @@ class UnderCensorship extends HTMLElement {
         shadow.append(...this.childNodes)
     }
 
-    #generateStyle(censorshipType: string, censorshipElement: string, censorshipColor = this.#defaultColor) {
+    #generateStyle(
+        censorshipType: string,
+        censorshipElement: string,
+        censorshipColor: string = this.#defaultColor
+    ): string {
         switch(censorshipType){
             case "paint":
                 return `${censorshipElement} { color: ${censorshipColor}; background-color: ${censorshipColor}; }`
@@ -83,12 +86,16 @@ class UnderCensorship extends HTMLElement {
         }
     }
 
-    #replaceText(searchText: string, newText: string, willRepeat: boolean) {
+    #replaceText(
+        searchText: string,
+        newText: string,
+        willRepeat: boolean
+    ): string {
         if (searchText == null || searchText == "" || newText == null || newText == "") {
             return ""
         }
 
-        let result = newText
+        let result: string = newText
         if (willRepeat) {
             while(searchText.length > result.length) {
                 result += newText
