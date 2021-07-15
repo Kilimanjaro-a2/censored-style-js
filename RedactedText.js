d7c812fb5d571cf1c1de063c1e71a415863abd37
Kili <kilimanjaro.a2@gmail.com>
2021-07-15 18:31:02 +0900

    Enabled to change colors

--- RedactedText.js
+++ RedactedText.js
@@ -5,17 +5,39 @@ class RedactedText extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" })
 
         const wrapper = document.createElement("span")
-        wrapper.setAttribute("class", "redacted")
 
         const slot = document.createElement("slot")
         wrapper.appendChild(slot)
 
         const style = document.createElement("style")
 
+        const type = this.getAttribute('type')
+        switch(type) {
+            case "red":
+                wrapper.setAttribute("class", "red")
+                break;
+            case "normal":
+            default:
+                wrapper.setAttribute("class", "normal")
+                break;
+        }
+
+        const color = this.getAttribute('color')
+        const regEx = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")
+        const isColorCode = regEx.test(color)
+
         style.textContent = `
-            .redacted {
+            .normal {
                 display: inline-block;
                 background-color: black;
+                color:black
+                padding: 0;
+                margin: 0; 
+            }
+            .red {
+                display: inline-block;
+                background-color: red;
+                color: red;
                 padding: 0;
                 margin: 0; 
             }
