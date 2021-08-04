26bc0a3ca097f902449432a0ebfeb85559c86b45
Kili <kilimanjaro.a2@gmail.com>
2021-08-04 17:24:10 +0900

    Using CSS variables to combine duplicate descriptions into one

--- src/style-util.ts
+++ src/style-util.ts
@@ -151,212 +151,105 @@ function generateSquareStyle (color: string, willHover: boolean = true) {
 }
 
 function generateCaligraphyStyle (willHover: boolean = true): string {
+  const commonProperties = `
+      --line-top: calc((100% - var(--line-height))/2);
+      position: absolute;
+      width: 100%;
+      height: var(--line-height);
+      top: calc(var(--line-top) - var(--line-top-offset));
+      left: 0;
+      transform: skew(-20deg);
+      
+      background-color: var(--color);
+      box-shadow: 0px 0px 2px 1px var(--color);
+      border-radius: 3px;
+      padding: 0 0.7em;
+      border-radius: var(--border-radius);
+  `
+
   const base: string = `
   .container {
     position: relative;
   }
   .paint-span {
     --color: black;
+    --line-top-offset: 0px;
     --line-height: 80%;
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 100%;
-    height: var(--line-height);
-    top: var(--line-top);
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 116px 284px 309px 145px / 102px 118px 58px 77px;
+    --border-radius: 116px 284px 309px 145px / 102px 118px 58px 77px;
+    ${commonProperties}
   }
   
   .paint-span-sub1 {
-    --line-height: 60%;
-    --line-top: calc((100% - var(--line-height))/2);
-    --line-skew-deg: -8deg;
+    --color: black;
     --line-top-offset: 4px;
-    position: absolute;
-    width: 100%;
-    height: var(--line-height);
-    top: calc(var(--line-top) - var(--line-top-offset));
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px transparent;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    --line-height: 60%;
+    --line-skew-deg: -8deg;
+    --border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    ${commonProperties}
   }
   
   .container:first-child .paint-span-sub1 {
+    --color: black;
     --line-top-offset: 1px;
     --line-height: 95%;
-    
-    --color: black;
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 50%;
-    height: var(--line-height);
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 116px 378px 299px 145px / 102px 148px 45px 77px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 116px 378px 299px 145px / 102px 148px 45px 77px;
+    ${commonProperties}
   }
   
   .container:first-child .paint-span-sub2 {
+    --color: rgba(0, 0, 0, 0.43);
     --line-top-offset: -3px;
     --line-height: 80%;
-    
-    --color: rgba(0, 0, 0, 0.43);
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 50%;
-    height: var(--line-height);
-    left: 10px;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    ${commonProperties}
   }
   
   .container:nth-child(2) .paint-span-sub1 {
     --color: rgba(0, 0, 0, 0.3);
     --line-top-offset: -2px;
-    
     --line-height: 80%;
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 100%;
-    height: var(--line-height);
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    ${commonProperties}
   }
   
   .container:nth-child(3n) .paint-span-sub1 {
     --color: rgba(0, 0, 0, 0.73);
     --line-top-offset: 4px;
-    
     --line-height: 80%;
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 100%;
-    height: var(--line-height);
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    ${commonProperties}
   }
   
   .container:nth-child(5n) .paint-span-sub1 {
     --color: rgba(0, 0, 0, 1);
     --line-top-offset: -10px;
-    
     --line-height: 3%;
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 30%;
-    height: var(--line-height);
-    left: 0;
-    transform: skew(-20deg);
-    
-    padding: 0 0.7em;
-    border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 112px 132px 68px 180px / 60px 85px 77px 90px;
+    ${commonProperties}
   }
   
-  
   .container:nth-child(4n) .paint-span-sub1 {
+    --color: rgba(0, 0, 0, 0.73);
     --line-top-offset: 0px;
     --line-height: 100%;
-    
-    --color: rgba(0, 0, 0, 0.73);
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 120%;
-    height: var(--line-height);
-    left: 0;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 50px 161px 334px 50px / 25px 66px 145px 25px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 50px 161px 334px 50px / 25px 66px 145px 25px;
+    ${commonProperties}
   }
   
   .container:nth-child(4n) .paint-span-sub2 {
+    --color: rgba(0, 0, 0, 0.43);
     --line-top-offset: 1px;
     --line-height: 100%;
-    
-    --color: rgba(0, 0, 0, 0.43);
-    --line-top: calc((100% - var(--line-height))/2);
     --line-skew-deg: -8deg;
-    position: absolute;
-    width: 120%;
-    height: var(--line-height);
-    left: -1px;
-    transform: skew(-20deg);
-    
-    background-color: transparent;
-    box-shadow: 0px 0px 2px 1px;
-    border-radius: 3px;
-    padding: 0 0.7em;
-    border-radius: 50px 374px 299px 50px / 25px 142px 45px 25px;
-  
-    background-color: var(--color);
-    box-shadow: 0px 0px 2px 1px var(--color);
-    top: calc(var(--line-top) - var(--line-top-offset));
+    --border-radius: 50px 374px 299px 50px / 25px 142px 45px 25px;
+    ${commonProperties}
   }
   `
   const hover: string = willHover
